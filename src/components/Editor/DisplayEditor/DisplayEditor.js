0b75c9f5dab2a19eb11fd9f66e1a3919a1ea70ac
Wm11 <56996174+Wm11@users.noreply.github.com>
2020-12-19 00:19:50 +0100

    Fix bugs in DisplayEditor

--- src/components/Editor/DisplayEditor/DisplayEditor.js
+++ src/components/Editor/DisplayEditor/DisplayEditor.js
@@ -2,22 +2,17 @@ import React from 'react'
 import './styleEditor.css'
 import MAP from '../EditorMap/MAP';
 import ToolBar from '../ToolBar/ToolBar';
-// import ChooseBar from '../ChooseBar/index';
 
 function DisplayEditor() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-2"><ToolBar /></div>
-        <div class="col-10"><MAP /></div>
+    <div className="container">
+      <div className="row">
+        <div className="col-2"><ToolBar /></div>
+        <div className="col-10"><MAP /></div>
         {/* <div class="w-100"></div>
-        <div class="col"><ChooseBar/></div> */}
+        <div class="col"><p>ChooseBar<p></div> */}
       </div>
     </div>
-    // <>
-    //     <MAP id='map' />
-    //     <Tool />
-    // </>
   );
 }
 
