25642f9d0c7f421de8d87efac510df91ac50c130
km100068 <74421466+km100068@users.noreply.github.com>
2021-01-15 15:30:46 +0100

    Add files via upload

---
+++ src/components/Editor/ChooseBar/ChooseBar.js
@@ -0,0 +1,9 @@
+//import React from 'react';
+function useChooseBar(userID) {
+
+    const userType = 0;     //0 - tryb mieszkańca;  1 - tryb przegladarki; 2- tryb urzednika
+  
+    return userType
+  }
+  
+  export default useChooseBar;
